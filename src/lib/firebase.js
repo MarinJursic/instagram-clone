52526933ee90b31bc4298441589dce13d55600c4
MarinJursic <marin.jursic@gmail.com>
2021-08-27 18:21:54 +0200

    env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyC868daBzUTyTCta0Z0DMbunDhqXa_1lk8",
-  authDomain: "instagram-yt-e40a2.firebaseapp.com",
-  projectId: "instagram-yt-e40a2",
-  storageBucket: "instagram-yt-e40a2.appspot.com",
-  messagingSenderId: "311665041675",
-  appId: "1:311665041675:web:a59d1e7103396c8b1e020e",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const firebase = Firebase.initializeApp(config);
